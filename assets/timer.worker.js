261248a19c6eea0f3586e19a9923ebe09327ac9d
totymedli <totymedli@users.noreply.github.com>
2026-05-04 08:10:57 +0000

    deploy: 901011f33d4fa64ef416eb121485b77653e8dba4

--- assets/timer.worker.js
+++
@@ -1,1 +0,0 @@
-const s=new Set,c=o=>{for(const t of s)t.postMessage(o)};self.onconnect=o=>{const t=o.ports[0];s.add(t),t.onmessage=a=>{const{type:e,payload:n}=a.data;e==="update"&&c(n)},t.start()};
